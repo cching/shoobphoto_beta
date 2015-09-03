afc2769323f1d610c1d21180d18967544894f2b7
= <cching@live.com>
2015-09-03 00:29:57 -0700

    fix strong params

--- app/controllers/admin/banners_controller.rb
+++ app/controllers/admin/banners_controller.rb
@@ -42,6 +42,6 @@ class Admin::BannersController < ApplicationController
     end
 
     def banner_params
-      params.require(:banner).permit(:school_id, :image)
+      params.require(:banner).permit(:school_id, :image, :school_type_ids)
     end
 end
