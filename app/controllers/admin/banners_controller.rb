abaa9147d77507f708beaa49f644af72370884b2
= <cching@live.com>
2015-09-03 00:22:57 -0700

    fixed banner

--- app/controllers/admin/banners_controller.rb
+++ app/controllers/admin/banners_controller.rb
@@ -42,6 +42,6 @@ class Admin::BannersController < ApplicationController
     end
 
     def banner_params
-      params.require(:banner).permit(:school_id, :banner)
+      params.require(:banner).permit(:school_id, :image)
     end
 end
