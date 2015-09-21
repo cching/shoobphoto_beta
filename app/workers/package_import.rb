cf2c13d053a680e68ef96d2012a4eb3211c197d4
= <cching@live.com>
2015-09-20 23:48:37 -0700

    added id column

--- app/workers/package_import.rb
+++ app/workers/package_import.rb
@@ -8,22 +8,17 @@ class PackageImport
       	chunk.each do |h|
 	        unless h["student_id"].nil?
 	        student = school.students.find_by_student_id("#{h["student_id"]}")
-	        s_id = StudentImage.last.id + 1
 
 	          unless student.present?     
 	            id = Student.last.id + 1
-	            student = school.students.new(:id => id, :student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"])
+	            student = school.students.new(:id => h["id"], :student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"])
 	            student.save
 	           else
 	           	student.update(:student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"])
 	          end
 
-	          image = package.student_images.new(:id => s_id, :student_id => student.id, :image_file_name => h["url"], :folder => h["folder"], :grade => h["grade"], :index_file_name => "h[url]-index")
-	          until image.save
-	          	s_id = StudentImage.last.id + 1
-	          	image = package.student_images.new(:id => s_id, :student_id => student.id, :image_file_name => h["url"], :folder => h["folder"], :grade => h["grade"], :index_file_name => "h[url]-index")
-	          end
-
+	          image = package.student_images.new(:id => h["id"], :student_id => student.id, :image_file_name => h["url"], :folder => h["folder"], :grade => h["grade"], :index_file_name => "h[url]-index")
+	          image.save
 	        end
      	end
  	end
