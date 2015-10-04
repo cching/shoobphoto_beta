bb408294f72f5ed3e054803dd92c114903f11e1a
= <cching@live.com>
2015-10-04 16:53:10 -0700

    force student id

--- app/workers/package_import.rb
+++ app/workers/package_import.rb
@@ -8,11 +8,12 @@ class PackageImport
       	chunk.each do |h|
 	        unless h["student_id"].nil?
 	        student = school.students.find_by_student_id("#{h["student_id"]}")
-
+	        sid = Student.last.id + 1
 	          unless student.present?     
-	            student = school.students.new(:student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
+	            student = school.students.new(:id => sid, :student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
 	            until student.save
-	            	student = school.students.new(:student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
+	            	sid = sid + 1
+	            	student = school.students.new(:id => sid, :student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
 	            end
 	           else
 	           	student.update(:student_id => h["student_id"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
