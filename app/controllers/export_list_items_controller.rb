bc9077d0a9e6603a24253054d338faeefd250bc8
= <cching@live.com>
2015-10-18 21:35:45 -0700

    Fix checking for students

--- app/controllers/export_list_items_controller.rb
+++ app/controllers/export_list_items_controller.rb
@@ -17,7 +17,9 @@ class ExportListItemsController < ApplicationController
     @package = @school.packages.where("name like ?", "%Fall%").last
       params[:student][:student_ids].each do |student_id, value|
         student = Student.find(student_id)
-        if @package.student_images.where(:student_id => params[:id]).any?
+        image = @package.student_images.where(:student_id => params[:id]).last
+        if image.any?
+          if AWS::S3::S3Object.new(bucket, "images/#{image.folder}/#{image.image_file_name}.jpg").exists?
           current_user.students << Student.find(student_id)
         end
       end
