90e16d0cd3ad2fdefea00e575558c5f3585143fc
= <cching@live.com>
2015-10-25 16:02:10 -0700

    Add all orders to download

--- app/workers/order_export.rb
+++ app/workers/order_export.rb
@@ -8,29 +8,26 @@ class OrderExport
       csv_file = ''
 
           csv_file << CSV.generate_line(Order.all.first.attributes.keys[0..12].map{|column| column} + Order.all.first.attributes.keys[14..21].map{|column| column} + ['Price'] + ['Student First Name'] + ['Student Last Name'] + ['Student Teacher'] + ['Student ID'] + ['Student Grade'] + ['Student SChool'] + ['Student DoB'] + ['Type'] + ['Package'] + ['8x10 | 5x7 | 3x5 | Wallets | Image CD | Name on Wallets | Retouching'] + ['CA Code'] + ['Senior Image'])
-            Order.where(:processed => false).each do |order|
+            Order.all.each do |order|
+
               order.cart.students.each do |student|
+
 
               if order.cart.order_packages.where(:student_id => student.id).count > 1
 
                 @string = order.cart.order_packages.where(:student_id => student.id).collect { |w| w.package.slug }.join(", ")
-              else
+                @string2 = order.cart.order_packages.where(:student_id => student.id).collect { |w| w.option.name[-1] }.join(", ")
+              elsif order.cart.order_packages.where(:student_id => student.id).count == 1
                 @string = ""
+                @string2 = ""
+
                 order.cart.order_packages.where(:student_id => student.id).each do |opackage|
                   @string = @string + "#{opackage.package.slug}"
-                end
-              end
-
-              unless order.cart.order_packages.where(:student_id => student.id).count == 0
-
-              if order.cart.order_packages.where(:student_id => student.id).count > 1
-
-                @string2 = order.cart.order_packages.where(:student_id => student.id).collect { |w| w.option.name[-1] }.join(", ")
-              else
-                @string2 = ""
-                order.cart.order_packages.where(:student_id => student.id).each do |opackage|
                   @string2 = @string2 + "#{opackage.option.name[-1]}"
-                end         
+                end
+              else
+                @string = ""
+                @string2 = ""
               end
               
               csv_file << CSV.generate_line(order.attributes.values[0..12] + order.attributes.values[14..21] + ["#{Order.price(order.id, student.id)}"] +
@@ -41,7 +38,7 @@ class OrderExport
                 
 
               ) 
-            end
+            
           end
           end
           file_name = Rails.root.join('tmp', "order_#{Time.now.day}-#{Time.now.month}-#{Time.now.year}_#{Time.now.hour}_#{Time.now.min}.csv");
