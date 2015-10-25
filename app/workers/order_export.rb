0e55c839521fd0144b02c80c22446f637fbb68f5
= <cching@live.com>
2015-10-25 02:07:47 -0700

    removed process

--- app/workers/order_export.rb
+++ app/workers/order_export.rb
@@ -8,7 +8,7 @@ class OrderExport
       csv_file = ''
 
           csv_file << CSV.generate_line(Order.all.first.attributes.keys[0..12].map{|column| column} + Order.all.first.attributes.keys[14..21].map{|column| column} + ['Price'] + ['Student First Name'] + ['Student Last Name'] + ['Student Teacher'] + ['Student ID'] + ['Student Grade'] + ['Student SChool'] + ['Student DoB'] + ['Type'] + ['Package'] + ['8x10 | 5x7 | 3x5 | Wallets | Image CD | Name on Wallets | Retouching'] + ['CA Code'])
-            Order.where(:processed => false).each do |order|
+            Order.all.each do |order|
               order.cart.students.each do |student|
 
               if order.cart.order_packages.where(:student_id => student.id).count > 1
@@ -23,16 +23,16 @@ class OrderExport
 
               if order.cart.order_packages.where(:student_id => student.id).count > 1
 
-                @string2 = order.cart.order_packages.where(:student_id => student.id).collect { |w| w.option.name[-1] }.join(", ")
+                @string2 = order.cart.order_packages.where(:student_id => student.id).collect { |w| w.option.try(:name)[-1] }.join(", ")
               else
                 @string2 = ""
                 order.cart.order_packages.where(:student_id => student.id).each do |opackage|
-                  @string2 = @string2 + "#{opackage.option.name[-1]}"
+                  @string2 = @string2 + "#{opackage.option.try(:name)[-1]}"
                 end         
               end
               
               csv_file << CSV.generate_line(order.attributes.values[0..12] + order.attributes.values[14..21] + ["#{Order.price(order.id, student.id)}"] +
-                ["#{student.first_name}"] + ["#{student.last_name}"] + ["#{student.teacher}"] + ["#{student.student_id}"] + ["#{student.grade}"] + ["#{student.school.name}"] + ["#{student.dob}"] + 
+                ["#{student.first_name}"] + ["#{student.last_name}"] + ["#{student.teacher}"] + ["#{student.student_id}"] + ["#{student.grade}"] + ["#{student.school.try(:name)}"] + ["#{student.dob}"] + 
                 
               
              [@string] + [@string2] + [Package.concat(order.id, student.id)] + [order.cart.school.try(:ca_code)]
