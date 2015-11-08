d453217a02375290c6819057956693b281af6f57
= <cching@live.com>
2015-11-08 00:34:45 -0800

    end order loop earlier

--- app/workers/order_export.rb
+++ app/workers/order_export.rb
@@ -10,6 +10,8 @@ class OrderExport
           csv_file << CSV.generate_line(Order.all.first.attributes.keys[0..12].map{|column| column} + Order.all.first.attributes.keys[14..21].map{|column| column} + ['Price'] + ['Student First Name'] + ['Student Last Name'] + ['Student Teacher'] + ['Student ID'] + ['Student Grade'] + ['Student SChool'] + ['Student DoB'] + ['Type'] + ['Package'] + ['8x10 | 5x7 | 3x5 | Wallets | Image CD | Name on Wallets | Retouching'] + ['CA Code'] + ['Senior Image'])
             Order.all.where.not(processed: true).order(:id).each do |order|
               order.cart.students.each do |student|
+                @string1 = ""
+                @string2 = ""
 
               if order.cart.order_packages.where(:student_id => student.id).count > 1
 
@@ -32,6 +34,7 @@ class OrderExport
                   @string2 = @string2 + "#{opackage.option.name[-1]}"
                 end         
               end
+              end
               
               csv_file << CSV.generate_line(order.attributes.values[0..12] + order.attributes.values[14..21] + ["#{Order.price(order.id, student.id)}"] +
                 ["#{student.first_name}"] + ["#{student.last_name}"] + ["#{student.teacher}"] + ["#{student.student_id}"] + ["#{student.grade}"] + ["#{student.school.name}"] + ["#{student.dob}"] + 
@@ -41,7 +44,7 @@ class OrderExport
                 
 
               ) 
-            end
+            
           end
           end
           file_name = Rails.root.join('tmp', "order_#{Time.now.day}-#{Time.now.month}-#{Time.now.year}_#{Time.now.hour}_#{Time.now.min}.csv");
