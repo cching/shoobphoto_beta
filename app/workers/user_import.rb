58cdade36819dc9c320759ba0599c521bff343b3
= <cching@live.com>
2015-11-30 02:12:26 -0800

    add full message

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -5,8 +5,8 @@ class UserImport
   	def perform(chunk)
 
       	chunk.each do |h|
-    f = Field.create(:id => h[:id], :x => h[:x], :y => h[:y], :width => h[:width], :height => [:height], :align => "#{h[:align]}", :column => "#{h[:column]}", :template_id => h[:template_id], :font_id => h[:font_id], :text_size => h[:text_size], :color => "#{h[:color]}", :spacing => h[:spacing], :name => "#{h[:name]}", :created_at => "#{h[:created_at]}", :updated_at => "#{h[:updated_at]}")
-    f.errors.each do |error|
+    f = Field.create(:id => h[:id].to_i, :x => h[:x].to_i, :y => h[:y].to_i, :width => h[:width].to_i, :height => [:height].to_i, :align => "#{h[:align]}", :column => "#{h[:column]}", :template_id => h[:template_id], :font_id => h[:font_id], :text_size => h[:text_size], :color => "#{h[:color]}", :spacing => h[:spacing], :name => "#{h[:name]}", :created_at => "#{h[:created_at]}", :updated_at => "#{h[:updated_at]}")
+    f.errors.full_messages.each do |error|
     	puts "#@@@@@@#{error}"
     end
   end
