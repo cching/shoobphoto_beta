d1ae2efb34a458af7f1b9a0c8d752538277104b4
= <cching@live.com>
2015-11-30 02:06:54 -0800

    errror message

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -6,7 +6,9 @@ class UserImport
 
       	chunk.each do |h|
     f = Field.create(:id => h[:id], :x => h[:x], :y => h[:y], :width => h[:width], :height => [:height], :align => "#{h[:align]}", :column => "#{h[:column]}", :template_id => h[:template_id], :font_id => h[:font_id], :text_size => h[:text_size], :color => "#{h[:color]}", :spacing => h[:spacing], :name => "#{h[:name]}", :created_at => "#{h[:created_at]}", :updated_at => "#{h[:updated_at]}")
-    puts "@@@@@ #{f.errors}"
+    f.errors.each do |error|
+    	puts "#@@@@@@#{error.error}"
+    end
   end
  	end
 end
