20e68e7c7d15a4005665338bb9af2a9a719a68d8
= <cching@live.com>
2015-11-30 02:08:28 -0800

    Add error message

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -7,7 +7,7 @@ class UserImport
       	chunk.each do |h|
     f = Field.create(:id => h[:id], :x => h[:x], :y => h[:y], :width => h[:width], :height => [:height], :align => "#{h[:align]}", :column => "#{h[:column]}", :template_id => h[:template_id], :font_id => h[:font_id], :text_size => h[:text_size], :color => "#{h[:color]}", :spacing => h[:spacing], :name => "#{h[:name]}", :created_at => "#{h[:created_at]}", :updated_at => "#{h[:updated_at]}")
     f.errors.each do |error|
-    	puts "#@@@@@@#{error.error}"
+    	puts "#@@@@@@#{error}"
     end
   end
  	end
