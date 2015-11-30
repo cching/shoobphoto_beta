b06f7f93b0046052a74a62c07eed11bd18854b3e
= <cching@live.com>
2015-11-30 01:57:34 -0800

    swap importers

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -5,13 +5,7 @@ class UserImport
   	def perform(chunk)
 
       	chunk.each do |h|
-
- 	            school = School.find_by_ca_code(h["ca_code"].to_s)
-	            unless school.nil?
-	           	school.users.create(:email => h["email"], :password => h["password"], :password_confirmation => h["password_confirmation"])
-	        end #end if
-	          	
-	        
-     	end #end chunk
+    Field.create(:id => h[:id], :x => h[:x], :y => h[:y], :width => h[:width], :height => [:height], :align => "#{h[:align]}", :column => "#{h[:column]}", :template_id => h[:template_id], :font_id => h[:font_id], :text_size => h[:text_size], :color => "#{h[:color]}", :spacing => h[:spacing], :name => "#{h[:name]}", :created_at => "#{h[:created_at]}", :updated_at => "#{h[:updated_at]}")
+  end
  	end
 end
