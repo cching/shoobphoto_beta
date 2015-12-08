77aa98a96f7ad04df37e233162599e894c68b856
= <cching@live.com>
2015-12-07 23:05:55 -0800

    Add cart export

--- app/workers/corder_export.rb
+++ app/workers/corder_export.rb
@@ -7,38 +7,33 @@ class CorderExport
 
       csv_file = ''
 
-          csv_file << CSV.generate_line(Corder.all.first.attributes.keys[0..12].map{|column| column} + Corder.all.first.attributes.keys[14..21].map{|column| column}  + ['School'] + ['Price'] + ['Items'])
-            Corder.where(:processed => false).each do |order|
+        csv_file << CSV.generate_line(['ID'] + ['School'] + ['Student'] + ['Price'] + ['Created at'] + ['Purchased?'] + ["Student found"])
+              Cart.where("created_at >= ?", 1.week.ago.utc).where.not(cart_type: nil).each do |cart|
+                cart.students.each do |student|
 
-              
-              
-              @string = ""
-                
-              order.cart.items.each do |item|
-                citem = order.cart.cart_items.where(:item_id => item.id).last
-                @string = @string + "#{item.number}, #{item.name}, #{citem.quantity}; "
-              end
-     
-                csv_file << CSV.generate_line(order.attributes.values[0..12] + order.attributes.values[14..21] + ["#{order.schools}"] + ["#{order.price.to_i}"] + [@string]
+        @price = 0
 
-              ) 
+          cart.order_packages.where(:student_id => student.id).each do |package|
+           @price = package.option.price(student.school.id) + @price
+          end
+
+          cart.order_packages.where(:student_id => student.id).each do |opackage|
+            package = opackage.package
+            if package.shippings.where(:school_id => cart.students[params[:i].to_i].school.id).any?
+            @price = @price + package.shippings.where(:school_id => @cart.students[params[:i].to_i].school.id).first.price
+            elsif package.shippings.where(:school_id => nil).any?
+            @price = @price + package.shippings.where(:school_id => nil).first.price
             
-          end
-          
-          file_name = Rails.root.join('tmp', "order_#{Time.now.day}-#{Time.now.month}-#{Time.now.year}_#{Time.now.hour}_#{Time.now.min}.csv");
-
-          File.open(file_name, 'wb') do |file|
-          
-            file.puts csv_file
+            end
           end
 
-          s3 = AWS::S3.new
+        csv_file << CSV.generate_line(["#{cart.id}"] + ["#{student.school.name}"] + ["$#{@price}"] + ["#{cart.created_at}"] + ["#{cart.purchased}"] + ["#[cart.id_supplied]"])
 
-          key = File.basename(file_name)
 
-          file = s3.buckets['shoobphoto'].objects["csvs/#{key}"].write(:file => file_name)
-          file.acl = :public_read
+      end
 
-          export.update(:file_path => key)
+
+
+
   end
 end
