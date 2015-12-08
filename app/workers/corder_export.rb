0b84fb6346c09d2dc3ccde9619504c2f276ccc94
= <cching@live.com>
2015-12-07 23:06:16 -0800

    Add cart export

--- app/workers/corder_export.rb
+++ app/workers/corder_export.rb
@@ -35,5 +35,21 @@ class CorderExport
 
 
 
+          
+          file_name = Rails.root.join('tmp', "order_#{Time.now.day}-#{Time.now.month}-#{Time.now.year}_#{Time.now.hour}_#{Time.now.min}.csv");
+
+          File.open(file_name, 'wb') do |file|
+          
+            file.puts csv_file
+          end
+
+          s3 = AWS::S3.new
+
+          key = File.basename(file_name)
+
+          file = s3.buckets['shoobphoto'].objects["csvs/#{key}"].write(:file => file_name)
+          file.acl = :public_read
+
+          export.update(:file_path => key)
   end
 end
