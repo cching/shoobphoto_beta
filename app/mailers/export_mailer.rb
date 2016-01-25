e6636062007e17a2e030456194f07ec350e83a26
= <cching@live.com>
2016-01-24 20:25:17 -0800

    Add students'

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -9,14 +9,14 @@ class ExportMailer < ActionMailer::Base
     @image_url = s3object.url_for(:read, :expires => 60.minutes, :use_ssl => true) 
     attachments["#{@export.file_path}.csv"] = open("#{@image_url}").read
 
-    unless @tkey.nil?
+    unless @key.nil?
       s3object = AWS::S3::S3Object.new(bucket, "zips/#{@key}") 
       @url = s3object.url_for(:read, :expires => 60.minutes, :use_ssl => true) 
       attachments["StudentImages.zip"] = open("#{@url}").read
     end
 
 
-    mail(:to => "awards@shoobphoto.com", :from => 'info@shoobphoto.com', :subject => "#{@export.school.name} - awards",
+    mail(:to => "cfching95@gmail.com", :from => 'info@shoobphoto.com', :subject => "#{@export.school.name} - awards",
      :cc => "#{@export.user.email}" )
   end
 
