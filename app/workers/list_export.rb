aa742b4c90c609bc3b585fe4b1b48dc66f80ece2
= <cching@live.com>
2016-01-24 19:18:34 -0800

    Add new zip

--- app/workers/list_export.rb
+++ app/workers/list_export.rb
@@ -11,8 +11,8 @@ class ListExport
       @any = false
 
       bucket = AWS::S3::Bucket.new('shoobphoto')
-      t = Tempfile.new("my-temp-filename-#{Time.now}")
-      Zip::OutputStream.open(t.path) do |z|
+      compressed_filestream = Zip::ZipOutputStream.write_buffer do |zos|
+
             
           csv_file << CSV.generate_line(['Student ID'] + ['First Name'] + ['Last Name'] + ['Grade'] + ['Teacher'] + ['Image'])
             export_list.user.students.each do |student|
@@ -20,37 +20,37 @@ class ListExport
                 @string = "#{package.student_images.where(:student_id => student.id).last.image.url}"
 
                 image = package.student_images.where(:student_id => student.id)
-                puts "1@@@@@ #{image.last}"
                 if image.last.try(:folder).nil? && (image.last.downloaded == false)
                   puts "2@@@@@ #{image.last}"
                     title = "#{student.last_name}_#{student.first_name}.jpg"
-                    z.put_next_entry("images/#{title}")
+                    zos.put_next_entry("images/#{title}")
                     url1_data = open(image.last.image.url)
-                    z.print IO.read(url1_data)
+                    z.osprint IO.read(url1_data)
+
                     image.last.update(:downloaded => true)
                     @any = true
                 end
 
               else
                 @string = ""
-              end
+              end ## end if
 
                 csv_file << CSV.generate_line(["#{student.student_id}"] + ["#{student.first_name}"] + ["#{student.last_name}"] + ["#{student.grade}"] + ["#{student.teacher}"] + [@string]
 
               ) 
              
-          end
+          end ## end loop
           
-      end
+      end ## end zip file
 
           if @any
             s3 = AWS::S3.new
 
             @tkey = "#{Time.now}-zip"
-            file = s3.buckets['shoobphoto'].objects["zips/#{@tkey}.zip"].write(:file => t.path)
-            file.acl = :public_read
+            compressed_filestream.rewind
+            bucket.put("zips/#{@tkey}.zip", compressed_filestream.read, {}, 'authenticated-read')
+
           end
-          t.close
           
           file_name = Rails.root.join('tmp', "#{export_list.title}_#{export_list.created_at}.csv");
 
