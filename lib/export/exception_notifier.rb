fd19de31a7371dbd1c0328a65d2e74605fc98f30
= <cching@live.com>
2016-03-03 18:01:26 -0800

    Auto include exception notifier

--- lib/export/exception_notifier.rb
+++ lib/export/exception_notifier.rb
@@ -1,8 +1,6 @@
 def self.default_ignore_exceptions
   [].tap do |exceptions|
     exceptions << ::ActiveRecord::RecordNotFound if defined? ::ActiveRecord::RecordNotFound
-    exceptions << ::AbstractController::ActionNotFound if defined? ::AbstractController::ActionNotFound
-    exceptions << ::ActionController::RoutingError if defined? ::ActionController::RoutingError
     exceptions << ::ActionController::UnknownFormat if defined? ::ActionController::UnknownFormat
     exceptions << ::ActionController::InvalidCrossOriginRequest if defined? ::ActionController::InvalidCrossOriginRequest
   end
