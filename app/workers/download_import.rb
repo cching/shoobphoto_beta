c9752bd8c845fae26453082c78b5486a819741f7
= <cching@live.com>
2016-03-28 00:31:01 -0700

    Add notice

--- app/workers/download_import.rb
+++ app/workers/download_import.rb
@@ -6,7 +6,8 @@ class DownloadImport
   		@school = School.find(school_id)
 
       	chunk.each do |h|
-      		students = @school.students.find_by_student_id(h["st_stu_id"])
+      		puts "@@@@@@ #{h["st_stu_id"]}"
+      		students = @school.students.find_by_student_id("#{h["st_stu_id"]}")
       		package = Package.find_by_slug(h["pricelist"])
 	    	if students.any?
 	           students.last.download_images.create(:shoob_id => h["id"], :package_id => package.try(:id), :year => h["year"], :folder => ["folder"], :url => ["st_id"])
