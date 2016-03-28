7a2c3b82ed8849b9e8a26207f13a3f0485efb2a5
= <cching@live.com>
2016-03-28 00:34:37 -0700

    Fix single string array of students

--- app/workers/download_import.rb
+++ app/workers/download_import.rb
@@ -9,7 +9,7 @@ class DownloadImport
       		students = @school.students.find_by_student_id("#{h["st_stu_id"]}")
       		package = Package.find_by_slug(h["pricelist"])
 	    	unless students.nil?
-	           students.last.download_images.create(:shoob_id => h["id"], :package_id => package.try(:id), :year => h["year"], :folder => ["folder"], :url => ["st_id"])
+	           students.download_images.create(:shoob_id => h["id"], :package_id => package.try(:id), :year => h["year"], :folder => ["folder"], :url => ["st_id"])
 	         end	          	
 	        
      	end #end chunk
