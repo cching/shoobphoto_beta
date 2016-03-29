f0f7c8c1d894a848aa40ad2a235b79be22c418ad
= <cching@live.com>
2016-03-28 22:21:28 -0700

    Remove already shown picture

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -29,8 +29,8 @@ class StudentsController < ApplicationController
     unless @found_image.nil?
       @student = @found_image.student
       @ids = @student.download_images.pluck(:id)
-      id = @ids - [@found_image.id]
-      @images = DownloadImage.where(id: id)
+      @ids = @ids - [@found_image.id]
+      @images = DownloadImage.where(id: @ids)
 
       @images.each do |image|
         if @images.where(:package_id => image.package.id).where(:year => image.year).count > 1
