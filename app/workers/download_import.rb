aab6ffb04abf46315259a1627177237e7836b105
Chris <cching@live.com>
2016-05-21 00:45:57 -0700

    fix url

--- app/workers/download_import.rb
+++ app/workers/download_import.rb
@@ -10,9 +10,11 @@ class DownloadImport
 	    	unless students.nil?
 	    		package = Package.find_by_slug("#{h["pricelist"]}")
 	    		if students.download_images.where(:folder => h["folder"]).any?
-	    			i = students.download_images.where(:folder => h["folder"]).last.update(:shoob_id => h["id"], :package_id => package.try(:id), :year => h["year"], :url => h["st_id"])
+	    			i = students.download_images.where(:folder => h["folder"]).last
+	    			i.update(:shoob_id => h["id"], :package_id => package.try(:id), :year => h["year"], :url => h["st_id"])
 	    		else
 		           i = students.download_images.create(:shoob_id => h["id"], :package_id => package.try(:id), :year => h["year"], :folder => h["folder"], :url => h["st_id"])
+		           
 		        end
 		        i.update(:image_file_name => i.url.downcase)
 	         end	          	
