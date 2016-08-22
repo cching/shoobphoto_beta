6fcf0f20f4b7c8290f6603656650bbd8911d7bd7
Chris <cching@live.com>
2016-08-21 18:38:36 -0700

    Add 15 item corders

--- app/workers/corder_export.rb
+++ app/workers/corder_export.rb
@@ -7,13 +7,14 @@ class CorderExport
 
       csv_file = ''
 
-          csv_file << CSV.generate_line(Corder.all.first.attributes.keys[0..13].map{|column| column} + Corder.all.first.attributes.keys[14..21].map{|column| column} + ['Grade'] + ['School'] + ['District'] + ['Price'] + ['Items1to5'] + ['Items 6 to 10'] + ['Processed'])
+          csv_file << CSV.generate_line(Corder.all.first.attributes.keys[0..13].map{|column| column} + Corder.all.first.attributes.keys[14..21].map{|column| column} + ['Grade'] + ['School'] + ['District'] + ['Price'] + ['Items1to5'] + ['Items 6 to 10'] + ['Items 11 to 15'] + ['Processed'])
             Corder.all.each do |order|
 
               
               
               @string1 = ""
               @string2 = ""
+              @string3 = ""
                 
               order.cart.items[0..4].each do |item|
                 citem = order.cart.cart_items.where(:item_id => item.id).last
@@ -27,8 +28,16 @@ class CorderExport
                 @string2 = @string2 + "#{item.number}, #{item.name}, #{citem.quantity}; "
               end
               end
+
+              if order.cart.items.count > 10
+
+              order.cart.items[10..14].each do |item|
+                citem = order.cart.cart_items.where(:item_id => item.id).last
+                @string3 = @string3 + "#{item.number}, #{item.name}, #{citem.quantity}; "
+              end
+              end
      
-                csv_file << CSV.generate_line(order.attributes.values[0..13] + order.attributes.values[14..21] + [order.try(:grade)] + ["#{order.schools}"] +["#{order.district}"] + ["#{order.price.to_i}"] + [@string1] + [@string2] + ["#{order.processed}"]
+                csv_file << CSV.generate_line(order.attributes.values[0..13] + order.attributes.values[14..21] + [order.try(:grade)] + ["#{order.schools}"] +["#{order.district}"] + ["#{order.price.to_i}"] + [@string1] + [@string2] + [@string3] + ["#{order.processed}"]
 
               ) 
             
