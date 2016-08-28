c83e4320dc0a3c314562e012f3e4396f4319e7b9
Chris <cching@live.com>
2016-08-27 22:25:02 -0700

    Add mobile

--- app/workers/order_export.rb
+++ app/workers/order_export.rb
@@ -8,9 +8,8 @@ class OrderExport
       csv_file = ''
 
           csv_file << CSV.generate_line(Order.all.first.attributes.keys[0..12].map{|column| column} + Order.all.first.attributes.keys[14..21].map{|column| column} + ['Price'] + ['Student First Name'] + ['Student Last Name'] + ['Student Teacher'] + ['Student ID']  + ['Student Grade'] + ['Student SChool']  + ['Type'] + ['Package'] + ['8x10 | 5x7 | 3x5 | Wallets | Image CD | Name on Wallets | Retouching'] + ['CA Code'] + ['Senior Image'] + ['Grad Image'] + ['Year'] + ['Extra Poses'] + ['Sheet Types'] + ['Yearbook Image']+ ['Access Code'])
-          date = Date.parse('July 15 2016')
 
-            Order.where("created_at > ?", "#{date}").order(:id).each do |order|
+            Order.all.where.not(processed: true).order(:id).each do |order|
               order.cart.students.each do |student|
 
                 @string1 = ""
