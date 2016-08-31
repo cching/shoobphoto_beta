5cc81d0616151bdf648e443547a2671e33749314
Chris <cching@live.com>
2016-08-31 16:27:02 -0700

    Add fix for corder export

--- app/workers/corder_export.rb
+++ app/workers/corder_export.rb
@@ -7,7 +7,7 @@ class CorderExport
 
       csv_file = ''
 
-          csv_file << CSV.generate_line(Corder.all.first.attributes.keys[0..13].map{|column| column} + Corder.all.first.attributes.keys[14..21].map{|column| column} + ['Grade'] + ['School'] + ['District'] + ['Price'] + ['Items1to5'] + ['Items 6 to 10'] + ['Items 11 to 15'] + ['Items 16 to 20'] + ['Items 21 to 25']+ ['Processed'])
+          csv_file << CSV.generate_line(Corder.all.first.attributes.keys[0..13].map{|column| column} + Corder.all.first.attributes.keys[14..21].map{|column| column} + ['Grade'] + ['School'] + ['District'] + ['Price'] + ['Items1to5'] + ['Items 6 to 10'] + ['Items 11 to 15'] + ['Items 16 to 20'] + ['Items 21 to 25'] + ['Items 26 to 29'] + ['Processed'])
             Corder.all.each do |order|
 
               
@@ -17,6 +17,7 @@ class CorderExport
               @string3 = ""
               @string4 = ""
               @string5 = ""
+              @string6 = ""
 
               @items = order.cart.items.order(:number)
                 
@@ -43,7 +44,7 @@ class CorderExport
 
               if @items.count > 15
 
-              @items.each do |item|
+              @items[15..19].each do |item|
                 citem = order.cart.cart_items.where(:item_id => item.id).last
                 @string4 = @string4 + "#{item.number}, #{item.name}, #{citem.quantity}; "
               end
@@ -56,8 +57,16 @@ class CorderExport
                 @string5 = @string5 + "#{item.number}, #{item.name}, #{citem.quantity}; "
               end
               end
+
+              if @items.count > 25
+
+              @items[25..29].each do |item|
+                citem = order.cart.cart_items.where(:item_id => item.id).last
+                @string5 = @string5 + "#{item.number}, #{item.name}, #{citem.quantity}; "
+              end
+              end
      
-                csv_file << CSV.generate_line(order.attributes.values[0..13] + order.attributes.values[14..21] + [order.try(:grade)] + ["#{order.schools}"] +["#{order.district}"] + ["#{order.price.to_i}"] + [@string1] + [@string2] + [@string3] + [@string4] + [@string5] + ["#{order.processed}"]
+                csv_file << CSV.generate_line(order.attributes.values[0..13] + order.attributes.values[14..21] + [order.try(:grade)] + ["#{order.schools}"] +["#{order.district}"] + ["#{order.price.to_i}"] + [@string1] + [@string2] + [@string3] + [@string4] + [@string5] + [@string6] + ["#{order.processed}"]
 
               ) 
             
