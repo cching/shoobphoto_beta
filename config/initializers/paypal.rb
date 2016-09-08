19388d11a0277e3bbcb20eda4ffc3a3cb4a25589
Chris <cching@live.com>
2016-09-07 17:00:47 -0700

    remove paypayl init

--- config/initializers/paypal.rb
+++
@@ -1,8 +0,0 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
-PayPal::SDK.logger = Rails.logger
-
-PayPal::SDK.configure(
-  :mode => "sandbox", # "sandbox" or "live"
-  :client_id => ENV['CLIENT_ID'],
-  :client_secret => ENV['CLIENT_SECRET'],
-  :ssl_options => { } )
