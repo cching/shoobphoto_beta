de1d934ce67648158c550c2a993dc4ff9b2ef30e
Chris <cching@live.com>
2016-09-20 01:09:37 -0700

    remove c email, add enroller

--- app/workers/items_export.rb
+++ app/workers/items_export.rb
@@ -2,34 +2,12 @@ class ItemsExport
   include Sidekiq::Worker
   sidekiq_options queue: "package_import"
     def perform(id)
-      export = Export.find(id)
-      require 'csv'
-
-      csv_file = ''
-
-        csv_file << CSV.generate_line(['Database ID'] + ['Name'] +  ['Number'] + ['Price'] + ['Image URL'] + ['Subcategory'] + ['Search terms'] + ['Size'] + ['Product Type'])
-          Item.all.order(:id).each do |item|
-
-
-              csv_file << CSV.generate_line(["#{item.id}"] + ["#{item.name}"] + ["#{item.number}"] + ["#{item.price}"] + ["#{item.main.url}"]  + ["#{item.subcategory.try(:name)}"] + ["#{item.searchterms.map(&:name).join(", ")}"])
-
-            
+      School.all.each do |school|
+        school.students.each do |student|
+          if student.student_images.where("lower(folder) like ?", "%fall2017%").any?
+            student.update(:enrolled => true)
           end
-        
-          file_name = Rails.root.join('tmp', "item_export#{Time.now.day}-#{Time.now.month}-#{Time.now.year}_#{Time.now.hour}_#{Time.now.min}.csv");
-
-          File.open(file_name, 'wb') do |file|
-          
-            file.puts csv_file
-          end
-
-          s3 = AWS::S3.new
-
-          key = File.basename(file_name)
-
-          file = s3.buckets['shoobphoto'].objects["csvs/#{key}"].write(:file => file_name)
-          file.acl = :public_read
-
-          export.update(:file_path => key)
+        end
+      end
     end
 end
