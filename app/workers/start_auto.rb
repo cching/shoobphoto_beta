9e2415ef06f7ab25ef06ac2b2aca3dd18865d826
Chris <cching@live.com>
2016-10-09 15:17:22 -0700

    Add async auto

---
+++ app/workers/start_auto.rb
@@ -0,0 +1,20 @@
+class StartAuto
+ 	include Sidekiq::Worker
+ 	sidekiq_options queue: "package_import"
+
+  	def perform(chunk)
+  	bucket = AWS::S3::Bucket.new('shoobphoto')
+    objects = bucket.objects.with_prefix('AutoCSV/import').collect(&:key).drop(1)
+
+    objects.each do |object|
+      csv_path  = "https://s3-us-west-1.amazonaws.com/shoobphoto/#{object}"
+
+      csv_file  = open(csv_path,'r')
+
+      chunk = SmarterCSV.process(csv_file, {:file_encoding =>'iso-8859-1'}) do |chunk|
+        PackageImport.perform_async(chunk, object)
+      end
+ 
+    end
+  	end
+  end
