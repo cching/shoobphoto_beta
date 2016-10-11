0c724beeca99b97ca77dafdfa687aa3e7697c22a
Chris <cching@live.com>
2016-10-10 21:42:49 -0700

    Add multiple csv

--- app/controllers/autos_controller.rb
+++ app/controllers/autos_controller.rb
@@ -4,7 +4,11 @@ class AutosController < ApplicationController
   respond_to :html
 
   def start_import
-      chunk = SmarterCSV.process("#{Rails.root}/public/csv/csv.csv", {:chunk_size => 5000}) do |chunk|
+      chunk = SmarterCSV.process("#{Rails.root}/public/csv/1.csv", {:chunk_size => 2000}) do |chunk|
+        PackageImport.perform_async(chunk)
+      end
+
+      chunk = SmarterCSV.process("#{Rails.root}/public/csv/2.csv", {:chunk_size => 2000}) do |chunk|
         PackageImport.perform_async(chunk)
       end
     
