fa994837706e12126769cc6d2868c81edf0e0a2c
Chris <cching@live.com>
2016-10-31 03:38:57 -0700

    Add order export unprocessed

--- app/workers/order_export.rb
+++ app/workers/order_export.rb
@@ -9,7 +9,7 @@ class OrderExport
 
           csv_file << CSV.generate_line(Order.all.first.attributes.keys[0..12].map{|column| column} + Order.all.first.attributes.keys[14..21].map{|column| column} + ['Price'] + ['Student First Name'] + ['Student Last Name'] + ['Student Teacher'] + ['Student ID']  + ['Student Grade'] + ['Student SChool']  + ['Type'] + ['Package'] + ['8x10 | 5x7 | 3x5 | Wallets | Image CD | Name on Wallets | Retouching'] + ['CA Code'] + ['Senior Image'] + ['Grad Image'] + ['Year'] + ['Extra Poses'] + ['Sheet Types'] + ['Yearbook Image']+ ['Access Code'])
 
-            Order.all.order(:id).each do |order|
+            Order.all.where.not(processed: true).order(:id).each do |order|
               #Order.where("created_at >= ?", Date.strptime("07/01/2016", "%m/%d/%Y")).each do |order|
               order.cart.students.each do |student|
 
