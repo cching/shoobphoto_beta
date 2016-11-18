8773461c74de6276c0d7ffec3bd7f44bc90a18ff
Chris <cching@live.com>
2016-11-18 03:57:19 -0800

    add auto watermark

--- app/workers/render_watermark.rb
+++ app/workers/render_watermark.rb
@@ -8,6 +8,33 @@ class RenderWatermark
       s3 = AWS::S3.new
       bucket1 = s3.buckets["shoobphoto"]
 
+      if student.download_images.any?
+        bucket = AWS::S3::Bucket.new('shoobphoto')
+        s3 = AWS::S3.new
+        bucket1 = s3.buckets["shoobphoto"]
+
+        student.download_images.each do |image|
+        unless image.image_file_name.nil? || image.image_file_name == ""
+          unless image.watermark.exists?
+          if AWS::S3::S3Object.new(bucket, "images/#{image.folder}/#{image.image_file_name.upcase}.jpg").exists?
+          image.update(:image_file_name => image.image_file_name.upcase)
+          obj1 = bucket.objects["images/#{image.folder}/#{image.image_file_name}.jpg"]
+          obj2 = bucket.objects["images/watermarks/download_images/#{image.id}/original/#{image.image_file_name}.jpg"]
+          obj1.copy_to(obj2)
+          image.update(:watermark_file_name => image.image_file_name)
+          image.watermark.reprocess!
+          elsif AWS::S3::S3Object.new(bucket, "images/#{image.folder}/#{image.image_file_name.downcase}.jpg").exists?
+          image.update(:image_file_name => image.image_file_name.downcase)
+          obj1 = bucket.objects["images/#{image.folder}/#{image.image_file_name}.jpg"]
+          obj2 = bucket.objects["images/watermarks/download_images/#{image.id}/original/#{image.image_file_name}.jpg"]
+          obj1.copy_to(obj2)
+          image.update(:watermark_file_name => image.image_file_name)
+          image.watermark.reprocess!
+        end #end if
+      end
+        end #end unless
+        end #end loop
+      end #end if
 
         school.packages.each do |package|
         images = student.student_images.where(:package_id => package.id)
