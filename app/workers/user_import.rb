643bf35467aa46622bb48bc125d78b84bde57ced
Chris <cching@live.com>
2017-01-03 02:25:13 -0800

    Change to int on school search

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -7,12 +7,16 @@ class UserImport
 
       	chunk.each do |h|
       		school = School.where(:ca_code => "#{h["nscode"]}")
+      		query = School.where("lower(name) like ?", "%#{h["school"].downcase[0..6]}%")
 
       		if school.any?
       			school = school.last
       			school.update(:scode => h["scode"].to_i)
+      		elsif query.count == 1
+      			query = query.last
+      			query.update(:scode => h["scode"].to_i)
       		else
-      			@user_id << h["nscode"]
+      			@user_id << "#{h["nscode"]}"
       		end
 	   
      	end #end chunk
