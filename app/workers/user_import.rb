6b661dab89c8cb955061cad418dfb255de7fd67f
Chris <cching@live.com>
2017-01-15 03:52:45 -0800

    Add columns and import for teachers

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -1,12 +1,7 @@
 class UserImport
  	include Sidekiq::Worker
  	sidekiq_options queue: "package_import"
-  encoding_options = {
-  :invalid           => :replace,  # Replace invalid byte sequences
-  :undef             => :replace,  # Replace anything not defined in ASCII
-  :replace           => '',        # Use a blank for those replacements
-  :universal_newline => true       # Always break lines with \n
-}
+
 
   	def perform(chunk)
   		@user_id = []
@@ -14,17 +9,17 @@ class UserImport
       	chunk.each do |h|
 
           school = School.where(:scode => h["scode"].to_i)
-          schools = School.where("ca_code like ?", "%#{h["nscode"]}%")
+          schools = School.where("ca_code like ?", "%#{h["nscode"].encode('UTF-8', :invalid => :replace, :undef => :replace)}%")
 
           if school.any?
             school = school.last
-            teacher = school.teachers.create(:name => "#{h["comptea"].encode(Encoding.find('ASCII'), encoding_options)}", :full_name => "#{h["comptea"].encode(Encoding.find('ASCII'), encoding_options)}", :teacher_id => h["cl_id"].to_i, :email => "#{h["email"].encode(Encoding.find('ASCII'), encoding_options)}")
+            teacher = school.teachers.create(:name => "#{h["teacher"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :full_name => "#{h["comptea"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :teacher_id => h["cl_id"].to_i, :email => "#{h["email"].encode('UTF-8', :invalid => :replace, :undef => :replace)}")
           elsif schools.count == 1
             schools = schools.last
-            teacher = school.teachers.create(:name => "#{h["comptea"].encode(Encoding.find('ASCII'), encoding_options)}", :full_name => "#{h["comptea"].encode(Encoding.find('ASCII'), encoding_options)}", :teacher_id => h["cl_id"].to_i, :email => "#{h["email"].encode(Encoding.find('ASCII'), encoding_options)}")
+            teacher = school.teachers.create(:name => "#{h["teacher"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :full_name => "#{h["comptea"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :teacher_id => h["cl_id"].to_i, :email => "#{h["email"].encode('UTF-8', :invalid => :replace, :undef => :replace)}")
             schools.update(:scode => h["scode"].to_i)
           else
-            school.teachers.create(:name => "#{h["comptea"].encode(Encoding.find('ASCII'), encoding_options)}", :full_name => "#{h["comptea"].encode(Encoding.find('ASCII'), encoding_options)}", :teacher_id => h["cl_id"].to_i, :email => "#{h["email"].encode(Encoding.find('ASCII'), encoding_options)}", :scode => h["scode"].to_i)
+            school.teachers.create(:name => "#{h["teacher"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :full_name => "#{h["comptea"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :teacher_id => h["cl_id"].to_i, :email => "#{h["email"].encode('UTF-8', :invalid => :replace, :undef => :replace)}", :scode => h["scode"].to_i)
           end
 
 
