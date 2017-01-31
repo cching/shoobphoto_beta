510ff34b0c3feaaad7d9ecb40c0b5e44bfda668a
Chris <cching@live.com>
2017-01-31 13:20:34 -0800

    Add legacy code for import

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -2,16 +2,13 @@ class Auto < ActiveRecord::Base
 	has_many :student_errors
 
 	  def self.upload
-	    if Dir.glob("J:/Diglab2010/Dbf/CSV/*.csv").any?
+	    if Dir.glob("/Volumes/6TB-J-12-13/Diglab2017/Dbf/csv/*.csv").any?
 
 	      #clear previous images from transfer
 
-	      FileUtils.rm_rf("C:/Sites/Rails/AutoCSV/public/csv/.", secure: true)
+	      FileUtils.rm_rf("/Users/alexshoob/load_station/csv/.", secure: true)
 
-	      Dir.mkdir("C:/Sites/Rails/AutoCSV/public/csv")
-
-	      @new_output = Output.create(:terminate => false)
-
+	      Dir.mkdir("/Users/alexshoob/load_station/csv")
 
 	      s3 = AWS::S3.new
 	      encoding_options = {
@@ -25,17 +22,14 @@ class Auto < ActiveRecord::Base
 	      i = 1 # set index counter
 	      student_index = 0
 
-	      load_id = SecureRandom.hex
+	      @load_id = SecureRandom.hex
 
-	      failed_filename = "#{@new_output.id}_failed.csv"
-	      output_filename = "#{@new_output.id}_output.csv"
-	          File.open("public/#{output_filename}", "w") do |output| #initiate output CSV 
-	            File.open("public/#{failed_filename}", "w") do |failed| #initiate failed CSV 
+	      failed_filename = "#{@load_id}_failed.csv"
+	      output_filename = "#{@load_id}_output.csv"
+	          File.open("/Users/alexshoob/load_station/#{output_filename}", "w") do |output| #initiate output CSV 
+	            File.open("/Users/alexshoob/load_station/#{failed_filename}", "w") do |failed| #initiate failed CSV 
 
-	              Dir.glob("J:/Diglab2010/Dbf/CSV/*.csv") do |fname|
-	                  File.open("public/csv/#{@new_output.id}_#{i}_process_csv.csv", "w") do |new_file|
-	                end
-	                File.rename fname, "public/csv/#{@new_output.id}_#{i}_process_csv.csv" #move file to process into local folder
+	              Dir.glob("/Volumes/6TB-J-12-13/Diglab2017/Dbf/csv/*.csv") do |fname|
 
 	                if i == 1 #set headers for first iteration
 
@@ -49,27 +43,35 @@ class Auto < ActiveRecord::Base
 
 	                      end
 
-	                        @csv = SmarterCSV.process("public/csv/#{@new_output.id}_#{i}_process_csv.csv", options={:file_encoding =>'iso-8859-1'}) do |chunk| #goes through file to upload images from local network
+	                        @csv = SmarterCSV.process(fname, options={:file_encoding =>'iso-8859-1'}) do |chunk| #goes through file to upload images from local network
 	                          chunk.each do |h|
 	                            student_index = student_index + 1
 	                            unless h["ca_code"].nil? 
-					      			schools = School.where("ca_code like ?", "%#{h["ca_code"]}%")
+					      			schools = School.where("ca_code like ?", "%#{h[:ca_code]}%")
 
 						      		if schools.any?
 						      			school = schools.last
-						      			packages = school.packages.where("lower(slug) like ?", "%#{h["slug"].downcase}%")
+						      			packages = school.packages.where("lower(slug) like ?", "%#{h[:slug].downcase}%")
 						      			if packages.any? 
 						      				package = packages.last
-				                            unless h["url"].nil? || h["url"] == ""
+				                            unless h[:url].nil? || h[:url] == ""
+				                            	url = "#{h[:url]}".gsub("\\","\/").to_s
+				                            	url_array = url.split("\/")
+				                            	url_path = "/Volumes/6TB-J-12-13"
 
-				                              if File.exists?("#{h["url"]}.jpg")
-				                                file_name = File.basename(h["url"])
+				                            	url_index = 1
+				                            	until url_array.count == url_index
+				                            		url_path = url_path + "/#{url_array[url_index]}"
+				                            		url_index += 1
+				                            	end
+				                              if File.exists?("#{url_path}.jpg")
+				                                file_name = File.basename(url_path)
 				                                basename = file_name.downcase
 
-				                                Auto.watermark_images(h["url"], h["folder"], basename, s3, student_index, i)
+				                                Auto.watermark_images(url_path, h[:folder], basename, s3, student_index, i)
 				                                @failed << false
 				                                @output << true
-				                                response = "#{basename}, #{h["student_id"]}, #{h["accesscode"]}, #{h["last_name"]}, #{h["first_name"]}, #{h["grade"]}, #{h["folder"]}, #{h["email"]}, #{h["dob"]}, #{h["teacher"]}, #{school.id}, #{package.id}, #{h["shoob_id"]}, #{h["rec_type"]}, #{load_id}".encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+				                                response = "#{basename}, #{h[:student_id]}, #{h[:accesscode]}, #{h[:last_name]}, #{h[:first_name]}, #{h[:grade]}, #{h[:folder]}, #{h[:email]}, #{h[:dob]}, #{h[:teacher]}, #{school.id}, #{package.id}, #{h[:shoob_id]}, #{h[:rec_type]}, #{@load_id}".encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
 				                                output << response.encode(Encoding.find('ASCII'), encoding_options)
 				                                output << "\n"
 				                                
@@ -77,7 +79,7 @@ class Auto < ActiveRecord::Base
 				                                #write error file
 				                                @failed << true
 				                                @output << false
-				                                response = "Unable to locate student image in local folder, #{h["url"]}, #{h["student_id"]}, #{h["acesscode"]}, #{h[last_name]}, #{h["first_name"]}, #{h["grade"]}, #{h["folder"]}, #{h["email"]}, #{h["dob"]}, #{h["teacher"]}, #{h["ca_code"]}, #{h["rec_type"]}, h#{load_id}"
+				                                response = "Unable to locate student image in local folder, #{url_path}, #{h[:student_id]}, #{h[:acesscode]}, #{h[:last_name]}, #{h[:first_name]}, #{h[:grade]}, #{h[:folder]}, #{h[:email]}, #{h[:dob]}, #{h[:teacher]}, #{h[:ca_code]}, #{h[:rec_type]}, h#{@load_id}"
 				                                failed << response.encode(Encoding.find('ASCII'), encoding_options)
 				                                failed << "\n"
 
@@ -90,7 +92,7 @@ class Auto < ActiveRecord::Base
 
 				                              @failed << false
 				                                @output << true
-				                                response = ", #{h["student_id"]}, #{h["accesscode"]}, #{h["last_name"]}, #{h["first_name"]}, #{h["grade"]}, #{h["folder"]}, #{h["email"]}, #{h["dob"]}, #{h["teacher"]}, #{school.id}, #{package.id}, #{h["shoob_id"]}, #{h["rec_type"]}, #{h["load_id"]}".encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+				                                response = ", #{h[:student_id]}, #{h[:accesscode]}, #{h[:last_name]}, #{h[:first_name]}, #{h[:grade]}, #{h[:folder]}, #{h[:email]}, #{h[:dob]}, #{h[:teacher]}, #{school.id}, #{package.id}, #{h[:shoob_id]}, #{h[:rec_type]}, #{@load_id}".encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
 				                                output << response.encode(Encoding.find('ASCII'), encoding_options)
 				                                output << "\n"
 
@@ -98,7 +100,8 @@ class Auto < ActiveRecord::Base
 				                        else #if no packages found for school 
 				                        	@failed << true
 			                                @output << false
-			                                response = "No package found with slug #{h["slug"]} for school with CA code #{["ca_code"]}, #{h["url"]}, #{h["student_id"]}, #{h["acesscode"]}, #{h[last_name]}, #{h["first_name"]}, #{h["grade"]}, #{h["folder"]}, #{h["email"]}, #{h["dob"]}, #{h["teacher"]}, #{h["ca_code"]}, #{h["rec_type"]}, #{load_id}"
+			                                response = "No package found with slug #{h["slug"]} for school with CA code #{["ca_code"]}, #{url_path}, #{h[:student_id]}, #{h[:acesscode]}, #{h[:last_name]}, #{h[:first_name]}, #{h[:grade]}, #{h[:folder]}, #{h[:email]}, #{h[:dob]}, #{h[:teacher]}, #{h[:ca_code]}, #{h[:rec_type]}, h#{@load_id}"
+
 			                                failed << response.encode(Encoding.find('ASCII'), encoding_options)
 			                                failed << "\n"
 
@@ -106,14 +109,14 @@ class Auto < ActiveRecord::Base
 				                    else #if no school found with ca code
 				                    	@failed << true
 		                                @output << false
-		                                response = "No school found with CA code #{["ca_code"]}, #{h["url"]}, #{h["student_id"]}, #{h["acesscode"]}, #{h[last_name]}, #{h["first_name"]}, #{h["grade"]}, #{h["folder"]}, #{h["email"]}, #{h["dob"]}, #{h["teacher"]}, #{h["ca_code"]}, #{h["rec_type"]}, #{load_id}"
+		                                response = "No school found with CA code #{["ca_code"]}, #{url_path}, #{h[:student_id]}, #{h[:acesscode]}, #{h[:last_name]}, #{h[:first_name]}, #{h[:grade]}, #{h[:folder]}, #{h[:email]}, #{h[:dob]}, #{h[:teacher]}, #{h[:ca_code]}, #{h[:rec_type]}, h#{@load_id}"
 		                                failed << response.encode(Encoding.find('ASCII'), encoding_options)
 		                                failed << "\n"
 				                    end
 				                else #if no ca_code
 				                	@failed << true
 	                                @output << false
-	                                response = "No CA code given, #{h["url"]}, #{h["student_id"]}, #{h["acesscode"]}, #{h[last_name]}, #{h["first_name"]}, #{h["grade"]}, #{h["folder"]}, #{h["email"]}, #{h["dob"]}, #{h["teacher"]}, #{h["ca_code"]}, #{h["rec_type"]}, #{load_id}"
+		                            response = "No CA code given, #{url_path}, #{h[:student_id]}, #{h[:acesscode]}, #{h[:last_name]}, #{h[:first_name]}, #{h[:grade]}, #{h[:folder]}, #{h[:email]}, #{h[:dob]}, #{h[:teacher]}, #{h[:ca_code]}, #{h[:rec_type]}, h#{@load_id}"
 	                                failed << response.encode(Encoding.find('ASCII'), encoding_options)
 	                                failed << "\n"
 				                end
@@ -128,19 +131,14 @@ class Auto < ActiveRecord::Base
 	                end #close output
 
 	      #cleanup and upload
-	      if @failed.include? true
-	        obj = s3.buckets['shoobphoto'].objects["AutoCSV/failed/#{failed_filename}"]
-	        obj.write(File.read("public/#{failed_filename}"))
-	      else
-	        File.delete("public/#{failed_filename}")
-	      end
+
 
 	      if @output.include? true
 	        obj = s3.buckets['shoobphoto'].objects["AutoCSV/output/#{output_filename}"]
-	        obj.write(File.read("public/#{output_filename}"))
+	        obj.write(File.read("/Users/alexshoob/load_station/#{output_filename}"))
 	      end  
 
-	     Launchy.open("https:///www.shoobphoto.com/autos/start_auto")
+	     #Launchy.open("https:///www.shoobphoto.com/autos/start_auto")
 	      end # end if any csv files exist
 	     
 	  end
@@ -152,23 +150,23 @@ class Auto < ActiveRecord::Base
         watermark = mark.resize_to_fit(img.rows, img.columns.to_f)
 
         img2 = img.dissolve(watermark, 0.75, 1, Magick::CenterGravity)
-        img2.write("#{Rails.root}/public/watermarks/#{basename}.jpg")
+        img2.write("/Users/alexshoob/load_station/watermarks/#{basename}.jpg")
 
         img3 = img.resize_to_fit(28, 35)
-        img3.write("#{Rails.root}/public/index/#{basename}.jpg")
+        img3.write("/Users/alexshoob/load_station/index/#{basename}.jpg")
 
         obj_watermark = s3.buckets['shoobphoto'].objects["images/processed_watermarks/#{folder}/#{basename}.jpg"] # no request made
-        File.open("#{Rails.root}/public/watermarks/#{basename}.jpg", "rb") do |watermarked_image|
+        File.open("/Users/alexshoob/load_station/watermarks/#{basename}.jpg", "rb") do |watermarked_image|
           obj_watermark.write(watermarked_image)
         end
 
         obj_index = s3.buckets['shoobphoto'].objects["images/processed_index/#{folder}/#{basename}.jpg"] # no request made
-        File.open("#{Rails.root}/public/index/#{basename}.jpg", "rb") do |index_image|
+        File.open("/Users/alexshoob/load_station/index/#{basename}.jpg", "rb") do |index_image|
           obj_index.write(index_image)
         end
 
-        File.delete("#{Rails.root}/public/watermarks/#{basename}.jpg")
-        File.delete("#{Rails.root}/public/index/#{basename}.jpg")
+        File.delete("/Users/alexshoob/load_station/watermarks/#{basename}.jpg")
+        File.delete("/Users/alexshoob/load_station/index/#{basename}.jpg")
 
         puts "Watermarking and indexing images for student #{student_index} of CSV #{i}. Uploading to S3..."
 
--- app/workers/auto_import.rb
+++ app/workers/auto_import.rb
@@ -7,7 +7,14 @@ class AutoImport
    		bucket = AWS::S3::Bucket.new('shoobphoto')
       	s3 = AWS::S3.new
       	chunk.each do |h|
-      		school = School.find(h["school_id"].to_i)
+      		unless h["ca_code"].nil? 
+      			schools = School.where("ca_code like ?", "%#{h["ca_code"]}%")
+
+      		if schools.any?
+      			school = schools.last
+      			packages = school.packages.where("lower(slug) like ?", "%#{h["slug"].downcase}%")
+      		if packages.any? 
+      			package = packages.last
 	      	unless h["rec_type"].nil? || h["rec_type"] == ""
 		        
 					unless h["student_id"].nil?
@@ -15,44 +22,60 @@ class AutoImport
 
 						@auto.increment!(:success_count) 
 
-				        unless students.any?    
+				          unless students.any?    
 				            student = school.students.new(:student_id => h["student_id"], :access_code => h["accesscode"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :id_only => true, :access_code => h["accesscode"])
-				            student.save
-				        else
-				           	student = students.last
-				           	student.update(:student_id => h["student_id"], :access_code => h["accesscode"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true, :access_code => h["accesscode"])
-				        end
-
-
-				        images = student.student_images.where(:package_id => 6).where("load_id like ?", "%#{h["load_id"]}%")
-				        unless images.any?
-				          	image = package.student_images.new(:load_id => h["load_id"],:student_id => student.id, :image_file_name => h["url"], :folder => h["folder"], :grade => h["grade"], :url => h["url"], :shoob_id => h["shoob_id"])
-				            image.index_file_name = "#{image.image_file_name}-index" 
-				            image.save
-				        else
-				        	image = images.last
-				        end
-
-				        senior_image = image.senior_images.create(:url => h["url"], :image_file_name => h["url"])
-
-			        	if senior_image.image.exists?
-			        		obj1 = bucket.objects["images/processed_watermarks/#{image.folder}/#{senior_image.image_file_name}.jpg"]
-			                obj2 = bucket.objects["images/watermarks/seniors/#{senior_image.id}/original/#{senior_image.image_file_name}.jpg"]
-			                obj1.copy_to(obj2)
-			                senior_image.update(:watermark_file_name => senior_image.image_file_name)
-			        	end
-			        end
-		    	else #else rectype
-			    	unless h["student_id"].nil?
-			        students = school.students.where(:student_id => "#{h["student_id"]}")
-
-			        @auto.increment!(:success_count)
-
-				          unless students.any?     
-				            student = school.students.new(:student_id => h["student_id"], :access_code => h["accesscode"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
 				            student.save
 				           else
 				           	student = students.last
+				           	student.update(:student_id => h["student_id"], :access_code => h["accesscode"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true, :access_code => h["accesscode"])
+				          end
+
+				          if h["rec_type"].downcase == 'master'
+				          	image = package.student_images.new(:student_id => student.id, :image_file_name => h["url"], :folder => h["folder"], :grade => h["grade"], :url => h["url"], :shoob_id => h["shoob_id"])
+				            image.index_file_name = "#{image.image_file_name}-index"
+				            image.save
+				          else
+
+				          	images = student.student_images.where(:package_id => 6)
+
+				          	unless images.any?
+				          		image = package.student_images.new(:student_id => student.id, :image_file_name => h["url"], :folder => h["folder"], :grade => h["grade"], :url => h["url"], :shoob_id => h["shoob_id"])
+				            	image.index_file_name = "#{image.image_file_name}-index"
+				            	image.save
+				            else
+				            	image = images.last
+				        	end
+				        	end
+
+				          senior_image = image.senior_images.create(:url => h["url"], :image_file_name => h["url"])
+
+				          unless senior_image.image.exists?
+				        		senior_image.update(:image_file_name => senior_image.image_file_name.downcase)
+				        	end
+
+				        	unless senior_image.image.exists?
+				        		senior_image.update(:image_file_name => senior_image.image_file_name.upcase)
+				        	end
+
+				        	if senior_image.image.exists?
+				        		obj1 = bucket.objects["images/processed_watermarks/#{image.folder}/#{senior_image.image_file_name}.jpg"]
+				                  obj2 = bucket.objects["images/watermarks/seniors/#{senior_image.id}/original/#{senior_image.image_file_name}.jpg"]
+				                  obj1.copy_to(obj2)
+				                  senior_image.update(:watermark_file_name => senior_image.image_file_name)
+				        	end
+
+
+			        end
+		    	else #else rectype
+			    	unless h["student_id"].nil?
+			        student = school.students.find_by_student_id("#{h["student_id"]}")
+
+			        @auto.increment!(:success_count)
+
+				          unless student.present?     
+				            student = school.students.new(:student_id => h["student_id"], :access_code => h["accesscode"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
+				            student.save
+				           else
 				           	student.update(:student_id => h["student_id"], :access_code => h["accesscode"], :last_name => h["last_name"], :first_name => h["first_name"], :grade => h["grade"], :email => h["email"], :teacher => h["teacher"], :shoob_id => h["shoob_id"], :id_only => true)
 				          end 
  
@@ -80,13 +103,19 @@ class AutoImport
 
 				    end#end if no student id
 			    end  # end rectype
+			else #else packages
+				@auto.student_errors.create(:priority => 0, :error_text => "#{h}", :error_description => "No package found with slug #{h["slug"]} for school #{h["ca_code"]} - check that the package exists for the school")
+				@auto.increment!(:failed_count)
+			end # end if no packages found
 
+			else
+				@auto.student_errors.create(:priority => 0, :error_text => "#{h}", :error_description => "No school found with CA code #{h["ca_code"]} - check that the CSV CA code matches exactly with the school's CA code on the site")
+		        @auto.increment!(:failed_count)
+		    end #end if no schools found
+		end #end unless ca_code nil
 
      	end #end chunk loop
 
 
  	end #end def
 end #end class
-
-
- 
