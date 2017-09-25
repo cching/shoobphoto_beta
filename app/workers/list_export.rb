a1fe32b31a7db27cf19749e2568d1fe9da5ffc8d
Chris <cching@live.com>
2017-09-24 21:45:34 -0700

    Add character trait export on CSV

--- app/workers/list_export.rb
+++ app/workers/list_export.rb
@@ -7,32 +7,31 @@ class ListExport
   
           csv_file_setup = ''
 
-          csv_file_setup << CSV.generate_line(['scode'] + ['name'] + ['abbrev'] + ['time_per'] + ['awd_date'] + ['rec_by'] + ['labelfile'] + ['award_id'])
+          csv_file_setup << CSV.generate_line(['scode'] + ['name'] + ['abbrev'] + ['time_per'] + ['awd_date'] + ['rec_by'] + ['labelfile'] + ['award_id'] + ['trait'])
 
             export_list.award_infos.where(:processed => false).each do |award|
-               csv_file_setup << CSV.generate_line(["#{export_list.user.school.scode}"] + ["#{award.award.title}"] + ["#{award.award.abbreviation}"] + ["#{award.time_period}"] + ["#{award.award_date}"]+  ["#{award.receive_by}"] + ["#{File.basename award.award.image_file_name, '.jpg'}"] + ["#{award.award.abbreviation.humanize}#{award.id}"])
+               csv_file_setup << CSV.generate_line(["#{export_list.user.school.scode}"] + ["#{award.award.title}"] + ["#{award.award.abbreviation}"] + ["#{award.time_period}"] + ["#{award.award_date}"]+  ["#{award.receive_by}"] + ["#{File.basename award.award.image_file_name, '.jpg'}"] + ["#{award.award.abbreviation.humanize}#{award.id}"] + ["#{award.awarded_for}"])
             end
           
 
           csv_file = ''
 
-          csv_file << CSV.generate_line(['scode'] + ['shoob_id'] + ['st_stu_id'] + ['st_fname'] + ['st_lname'] + ['st_grade'] + ['st_teacher'] + ['image'] + ['award'] + ['st_id'])
+          csv_file << CSV.generate_line(['scode'] + ['shoob_id'] + ['st_stu_id'] + ['st_fname'] + ['st_lname'] + ['st_grade'] + ['st_teacher'] + ['image'] + ['award'] + ['st_id'] + ['trait'])
 
             export_list.award_infos.where(:processed => false).each do |award|
-              award.students.each do |student|
+              award.award_info_students.each do |award_student|
                 @image = student.school.packages.where(hidden: false).where("name like ?", "%Fall%").last
 
                 if @image.nil?
-                  @image = student.school.packages.first
+                  @image = award_student.student.school.packages.first
                 end
-                img = @image.student_images.where(:student_id => student.id)
-                @string = ""
-                @string2 = ""
+                img = @image.student_images.where(:student_id => award_student.student_id)
+                @string = @string2 = ""
                 if img.any?
                   @string2 = "#{img.last.image.url}"
                   @string = "#{img.last.try(:image_file_name)}"
                 end
-               csv_file << CSV.generate_line(["#{student.school.scode}"] + ["#{student.shoob_id}"] + ["#{student.student_id}"] + ["#{student.first_name}"] + ["#{student.last_name}"] + ["#{student.grade}"] + ["#{student.teacher}"] + ["#{@string2}"] + ["#{File.basename award.award.image_file_name, '.jpg'}"] + ["#{@string}"])
+               csv_file << CSV.generate_line(["#{award_student.student.school.scode}"] + ["#{award_student.student.shoob_id}"] + ["#{award_student.student.student_id}"] + ["#{award_student.student.first_name}"] + ["#{award_student.student.last_name}"] + ["#{award_student.student.grade}"] + ["#{award_student.student.teacher}"] + ["#{@string2}"] + ["#{File.basename award.award.image_file_name, '.jpg'}"] + ["#{@string}"] + ["#{award_student.trait}"])
              end
             end
           
