fa45d23e0450ba7954dba9b3380673f40e569750
ep <shoobphotoep@gmail.com>
2017-10-29 21:41:34 -0700

    strip whitespace

--- app/workers/user_import.rb
+++ app/workers/user_import.rb
@@ -13,7 +13,7 @@ class UserImport
 
       	chunk.each do |h|
           item = Item.find(h["id"])  
-          schools = School.where(ca_code: "#{h["ca_code"]}")
+          schools = School.where(ca_code: "#{h["ca_code"].strip}")
 
           if schools.any?
             schools.update_all(scode: "#{h["scode"]}")
