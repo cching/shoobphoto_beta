b541d6c874d227fcefb07c6a61b122167bbb13e4
ep <shoobphotoep@gmail.com>
2017-11-01 20:46:46 -0700

    Escape senior image on addons

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -293,16 +293,20 @@ class StudentsController < ApplicationController
 
     @opackage = @cart.order_packages.where(:package_id => 6).last
 
-    @s_image = @opackage.student.student_images.where(:student_id => @student.id).last
+    @package = @opackage.package
+    student_image = @package.student_images.where(:student_id => @student.id)
 
-    unless @s_image.nil?
+    if student_image.any?
+      @s_image = student_image.last
       @senior_images = @s_image.senior_images.paginate(:per_page => 4, :page => params[:page])
     end
+
+    image = @package.student_images.where(:student_id => @opackage.student.id).last
 
     @package = @opackage.package
     image = @package.student_images.where(:student_id => @opackage.student.id).last
 
-        if @package.id == 6 && image.student_images.any? && @cart.id_supplied?
+        if @package.id == 6 && image && @cart.id_supplied?
           @boolean = false
           image.senior_images.each do |senior_image|
             if senior_image.watermark.exists?
