7f1e82748da0cb2ff3ecb88aa221e7ab883da5c6
Ethan <ethan@alexs-mbp.attlocal.net>
2017-11-22 12:34:06 -0800

    info@shoobphoto award function removed

--- app/mailers/export_mailer.rb
+++ app/mailers/export_mailer.rb
@@ -1,18 +1,18 @@
-class ExportMailer < ActionMailer::Base
-  helper :application
-  
-  def send_mail args
-    @export = args
-    bucket = AWS::S3::Bucket.new('shoobphoto') 
-    s3object = AWS::S3::S3Object.new(bucket, "csvs/#{@export.file_path}") 
-    s3object_setup = AWS::S3::S3Object.new(bucket, "csvs/#{@export.file_path}-setup") 
-    @image_url = s3object.url_for(:read, :expires => 60.minutes, :use_ssl => true) 
-    @image_url_setup = s3object_setup.url_for(:read, :expires => 60.minutes, :use_ssl => true) 
-    attachments["#{@export.file_path}.csv"] = open("#{@image_url}").read
-    attachments["#{@export.file_path}-setup.csv"] = open("#{@image_url_setup}").read
-
-    mail(:to => "awards@shoobphoto.com", :from => 'info@shoobphoto.com', :subject => "#{@export.user.school.name} - awards",
-     :cc => "#{@export.user.email}" )
-  end
-
-end 
+#class ExportMailer < ActionMailer::Base
+#  helper :application
+#  
+#  def send_mail args
+#    @export = args
+#    bucket = AWS::S3::Bucket.new('shoobphoto') 
+#    s3object = AWS::S3::S3Object.new(bucket, "csvs/#{@export.file_path}") 
+#    s3object_setup = AWS::S3::S3Object.new(bucket, "csvs/#{@export.file_path}-setup") 
+#    @image_url = s3object.url_for(:read, :expires => 60.minutes, :use_ssl => true) 
+#    @image_url_setup = s3object_setup.url_for(:read, :expires => 60.minutes, :use_ssl => true) 
+#    attachments["#{@export.file_path}.csv"] = open("#{@image_url}").read
+#    attachments["#{@export.file_path}-setup.csv"] = open("#{@image_url_setup}").read
+#
+#    mail(:to => "awards@shoobphoto.com", :from => 'info@shoobphoto.com', :subject => "#{@export.user.school.name} - awards",
+#     :cc => "#{@export.user.email}" )
+#  end
+#
+#end 
