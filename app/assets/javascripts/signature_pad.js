e67d93606695e0612f0ff06172014029126d336a
Mukesh Sachdev <mukesh.sachdev@ifuturz.com>
2018-08-09 11:49:56 +0530

    second definined const for cure

--- app/assets/javascripts/signature_pad.js
+++ app/assets/javascripts/signature_pad.js
@@ -409,7 +409,7 @@ SignaturePad.prototype._fromData = function (pointGroups, drawCurve, drawDot) {
           this._addPoint(point);
         } else if (j !== group.length - 1) {
           // Middle point in a group.
-          const { curve, widths } = this._addPoint(point);
+          const  curve = this._addPoint(point) , widths  = this._addPoint(point);
           if (curve && widths) {
             drawCurve(curve, widths);
           }
