44f7311f03974b81c9967cb5381f09b47f5781e4
chinganator <XDaniel@Daniels-MacBook-Air.local>
2018-08-09 14:53:49 -0700

    adds status column to table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808200904) do
+ActiveRecord::Schema.define(version: 20180809213952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -322,6 +322,7 @@ ActiveRecord::Schema.define(version: 20180808200904) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "school_id"
+    t.string   "status"
   end
 
   create_table "download_images", force: true do |t|
