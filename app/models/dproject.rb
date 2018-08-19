4f8795609f67fd2afa47d45f0c0dbebfdc384507
Ethan <ethan@Alexs-MacBook-Pro.local>
2018-08-19 13:44:34 -0700

    Make edit screen attachment changes

--- app/models/dproject.rb
+++ app/models/dproject.rb
@@ -19,11 +19,6 @@ class Dproject < ActiveRecord::Base
 
 	scope :by_school, -> {joins(:school).reorder('schools.name')}
 
-	has_attached_file :dfile,
-  	:url => ':s3_domain_url',
-  	:path => '/images/projects/:id/:filename'
-  	do_not_validate_attachment_file_type :dfile
-
 	def previous_dproject
   		Dproject.where(["id < ?", id]).last
 	end
