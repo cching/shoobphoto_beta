44d5c00432ca5a9b392cc722b03b2b09db108032
chinganator <XDaniel@Daniels-MacBook-Air.local>
2018-10-17 16:13:32 -0700

    editing delivered projects now works

--- app/models/dproject.rb
+++ app/models/dproject.rb
@@ -3,11 +3,18 @@ class Dproject < ActiveRecord::Base
 	belongs_to :school
 	
 	before_save :set_status_date
-    before_save :update_change_log
+	before_save :update_change_log
+	
+	validates :school_id, numericality: { other_than: 999, :message => "cannot be blank" }
+	validates_presence_of :assigned_to
+
+	scope :by_school, -> {joins(:school).reorder('schools.name')}
 
 	def sequential_dproject(q, direction)
-		ordered_dprojects = Dproject.ransack(q)
+		ordered_dprojects = Dproject.where.not(status: 'Delivered').ransack(q)
 		dprojects = ordered_dprojects.result.includes(:school)
+
+		dprojects = Dproject.where(status: 'Delivered') if dprojects.empty?
 		
 		index = dprojects.index self
 		next_index = index + direction
@@ -17,7 +24,6 @@ class Dproject < ActiveRecord::Base
 		dprojects[next_index]
 	end
 	
-
 	def set_status_date
 	  self.status_date = Time.now if status_changed?
 	end
@@ -26,10 +32,6 @@ class Dproject < ActiveRecord::Base
       self.change_log = self.change_log.to_s + "\n" + self.status.to_s + " " + self.status_date.try(:strftime, '%b %e, %Y').to_s if status_changed?
     end
 
-	validates :school_id, numericality: { other_than: 999, :message => "cannot be blank" }
-	validates_presence_of :assigned_to
-
-	scope :by_school, -> {joins(:school).reorder('schools.name')}
 
 	def previous_dproject
   		Dproject.where(["id < ?", id]).last
