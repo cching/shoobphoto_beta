de9c2c14355646ab4a5415574aa919cca3cc0b34
chinganator <XDaniel@Daniels-MacBook-Air.local>
2018-10-18 14:47:42 -0700

    Revert "editing delivered projects now works"
    This reverts commit 44d5c00432ca5a9b392cc722b03b2b09db108032.

--- app/models/dproject.rb
+++ app/models/dproject.rb
@@ -3,18 +3,11 @@ class Dproject < ActiveRecord::Base
 	belongs_to :school
 	
 	before_save :set_status_date
-	before_save :update_change_log
-	
-	validates :school_id, numericality: { other_than: 999, :message => "cannot be blank" }
-	validates_presence_of :assigned_to
-
-	scope :by_school, -> {joins(:school).reorder('schools.name')}
+    before_save :update_change_log
 
 	def sequential_dproject(q, direction)
-		ordered_dprojects = Dproject.where.not(status: 'Delivered').ransack(q)
+		ordered_dprojects = Dproject.ransack(q)
 		dprojects = ordered_dprojects.result.includes(:school)
-
-		dprojects = Dproject.where(status: 'Delivered') if dprojects.empty?
 		
 		index = dprojects.index self
 		next_index = index + direction
@@ -24,6 +17,7 @@ class Dproject < ActiveRecord::Base
 		dprojects[next_index]
 	end
 	
+
 	def set_status_date
 	  self.status_date = Time.now if status_changed?
 	end
@@ -32,6 +26,10 @@ class Dproject < ActiveRecord::Base
       self.change_log = self.change_log.to_s + "\n" + self.status.to_s + " " + self.status_date.try(:strftime, '%b %e, %Y').to_s if status_changed?
     end
 
+	validates :school_id, numericality: { other_than: 999, :message => "cannot be blank" }
+	validates_presence_of :assigned_to
+
+	scope :by_school, -> {joins(:school).reorder('schools.name')}
 
 	def previous_dproject
   		Dproject.where(["id < ?", id]).last
