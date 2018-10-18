21dd42ec1781ef5c4cba477d5c29d1bb03ea41cd
Ethan <ethan@Alexs-MacBook-Pro.local>
2018-10-18 12:29:00 -0700

    csv change 3

--- app/workers/corder_export.rb
+++ app/workers/corder_export.rb
@@ -7,73 +7,20 @@ class CorderExport
     
     csv_file = ''
 
-    csv_file << CSV.generate_line(['Items1to5'] + ['Items 6 to 10'] + ['Items 11 to 15'] + ['Items 16 to 20'] + ['Items 21 to 25'] + ['Items 26 to 30'] + ['Items 31 to 35'] + ['Items 36 to 40'])
-      Corder.all.each do |order|
-        @string1 = ""
-        @string2 = ""
-        @string3 = ""
-        @string4 = ""
-        @string5 = ""
-        @string6 = ""
-        @string7 = ""
-        @string8 = ""
+    Corder.all.each do |order|
+      @string1 = ""
 
-        @items = order.cart.items.order(:number)
 
-        @items[0..4].each do |item|
+      @items = order.cart.items.order(:number)
+
+      @i = 0
+
+        @items[0..39].each do |item|
           citem = order.cart.cart_items.where(:item_id => item.id).last
-          @string1 = @string1 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
+          @string1 = @string1 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}\n"
         end
+        @i = @i + 1
 
-        if @items.count > 5
-          @items[5..9].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string2 = @string2 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-
-        if @items.count > 10
-          @items[10..14].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string3 = @string3 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-
-        if @items.count > 15
-          @items[15..19].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string4 = @string4 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-
-        if @items.count > 20
-          @items[20..24].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string5 = @string5 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-
-        if @items.count > 25
-          @items[25..29].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string6 = @string6 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-
-        if @items.count > 30
-          @items[30..34].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string7 = @string7 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-
-        if @items.count > 35
-          @items[35..39].each do |item|
-            citem = order.cart.cart_items.where(:item_id => item.id).last
-            @string8 = @string8 + "#{item.number}, #{item.name}, #{citem.quantity}, #{render_quantity(citem)}; "
-          end
-        end
-     
         csv_file << CSV.generate_line([@string1] + [@string2] + [@string3] + [@string4] + [@string5] + [@string6] + [@string7] + [@string8])
       end
           
@@ -81,7 +28,7 @@ class CorderExport
 
       File.open(file_name, 'wb') do |file|
         file.puts csv_file
-      end
+    end
           
       s3 = AWS::S3.new
           
