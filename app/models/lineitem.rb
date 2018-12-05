e68b928bcb09440ae1d5377d87cffa505ce131d1
chinganator <XDaniel@Daniels-MacBook-Air.local>
2018-12-05 04:34:58 -0800

    Revert "readding invoices"
    This reverts commit 4e4242353591ff35ec4d67742b83349b78dae89e.

--- app/models/lineitem.rb
+++ app/models/lineitem.rb
@@ -1,15 +1,3 @@
 class Lineitem < ActiveRecord::Base
-    belongs_to :invoice
-    belongs_to :dprojects
-     
-     def total
-        self.extended_price ||=0.0 
-        self.extended_price = price * quantity
-    end 
-    def tax
-        self.sales_tax = total * 0.0725
-    end 
-    def final 
-        self.final_price = total + tax
-    end 
+    belongs_to :dproject
 end
